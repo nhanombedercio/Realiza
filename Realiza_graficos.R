6145a58ee959943c27b2e7fb4cfb29764b1765f4
Dercio Justino Nhanombe <75586184+nhanombedercio@users.noreply.github.com>
2022-11-03 17:16:52 +0200

    actualizacao

--- Realiza_graficos.R
+++ Realiza_graficos.R
@@ -29,64 +29,83 @@ localdate <- Resultados_Realiza %>%
 
  
 ggplot(donaNegocio) +
-  aes(x = Dona_negocio, y = n, fill = Dona_negocio, label=n  ) +
-  geom_col()+geom_text()  + ylab("Nr Empreendedoras")  + theme_bw() + 
+  aes(x = Dona_negocio, y = n, fill = Dona_negocio  ) +
+  geom_col()+ 
+  geom_text(aes(label=n), size = 3, position = position_stack(vjust = 0.5))+
+  scale_fill_hue(direction = 1) + ylab("Nr Empreendedoras") + xlab("")  + theme_bw() + 
   theme(
     plot.title = element_text(face = "bold", size = 12),
     legend.background = element_rect(fill = "white", size = 4, colour = "white"),
-     
+    legend.position="bottom",
     axis.ticks = element_line(colour = "grey70", size = 0.2),
     panel.grid.major = element_line(colour = "grey70", size = 0.2),
     panel.grid.minor = element_blank()
-  )+ggtitle("Are you a business owner?")
+  )+   guides(fill=guide_legend("Are you a business owner?"))
+  
+  
+  
 
  
 localdate %>%
  filter(Dona_negocio %in% "SIM") %>%
  ggplot() +
- aes(x = local, y = n, fill = local, label=n ) +
- geom_col(stat='identity', position = 'dodge') +geom_text()+
- scale_fill_hue(direction = 1)+ ylab("Nr Empreendedoras") + xlab("Cidade")  + theme_bw() + 
+ aes(x = local, y = n, fill = local ) +
+ geom_col(stat='identity', position = 'dodge') +
+  geom_text(aes(label=n), size = 4, position = position_stack(vjust = 0.5))+
+  scale_fill_hue(direction = 1) + ylab("Nr Empreendedoras")   + theme_bw() + 
   theme(
     plot.title = element_text(face = "bold", size = 12),
     legend.background = element_rect(fill = "white", size = 4, colour = "white"),
-    
+    legend.position="bottom",
     axis.ticks = element_line(colour = "grey70", size = 0.2),
     panel.grid.major = element_line(colour = "grey70", size = 0.2),
     panel.grid.minor = element_blank()
-  )+ggtitle("Businesswoman's Residence")
+  )+scale_y_continuous(limits=c(0,6000))+ggtitle("business owners by city")+guides(fill=guide_legend("Cidade de residencia"))
+  
+ 
 
 
 #Formalizacao vs local  e lucro vs local 
 formal <- Resultados_Realiza %>%
   filter(Dona_negocio %in% "SIM"  ) %>%
   group_by(local,  registo_negocio) %>%
-  summarise(n = n()) %>%   mutate(perc=n/sum(n))    
+  summarise(n = n()) %>%   mutate(perc=n/sum(n)) %>% 
+  mutate(percentagem=round(perc, digits = 3)*100 )    
    
 
 #Formalizacao vs local  e lucro vs local 
 lucro <- Resultados_Realiza %>%
   filter(Dona_negocio %in% "SIM"  ) %>%
   group_by(local,  Lucro) %>%
-  summarise(n = n()) %>%   mutate(perc=(n/sum(n))*100)    
+  summarise(n = n()) %>%   mutate(perc=(n/sum(n))*100) %>% 
+   mutate(percentagem=round(perc, digits = 3) )
 
 
  
 
 ggplot(formal) +
- aes(x = local, y = round(perc, digits = 2), fill = registo_negocio,group =registo_negocio,label=round(perc*100, digits = 1))+
-  geom_col() + 
-  geom_text(size = 3, position = position_stack(vjust = 0.5))+
-  +
- scale_fill_hue(direction = 1) + ylab("Percentagem") + xlab("Cidade")  + theme_bw() + 
+ aes(x = local, y =percentagem , fill = registo_negocio,group =registo_negocio)+
+  geom_col() + xlab("")+ 
+  geom_text(aes(label=percentagem), size = 4, position = position_stack(vjust = 0.5))+
+  scale_fill_hue(direction = 1) + ylab("Percentagem") 
++ theme_bw() + 
   theme(
     plot.title = element_text(face = "bold", size = 12),
     legend.background = element_rect(fill = "white", size = 4, colour = "white"),
-    legend.position="top",
+    legend.position="bottom",
     axis.ticks = element_line(colour = "grey70", size = 0.2),
     panel.grid.major = element_line(colour = "grey70", size = 0.2),
     panel.grid.minor = element_blank()
-  )+ggtitle("Business is formal ")
+  )+
+  ggtitle("Business is formal")
++guides(fill=guide_legend(""))
+
+
+
+  
+  
+  
+  
 
 ##lucro
 
@@ -96,13 +115,15 @@ ggplot(lucro) +
  aes(x = local, y = round(perc, digits = 1), fill = Lucro) +
  geom_col() + 
   geom_text(aes(label=round(perc, digits = 1)), size = 3, position = position_stack(vjust = 0.5))+
-  scale_fill_hue(direction = 1) + ylab("Percentagem") + xlab("Cidade")  + theme_bw() + 
+  scale_fill_hue(direction = 1) + ylab("Percentagem") + 
+  xlab("Lucro das empreendedoras") + theme_bw() + 
   theme(
     plot.title = element_text(face = "bold", size = 12),
     legend.background = element_rect(fill = "white", size = 4, colour = "white"),
-    legend.position="top",
+    legend.position="bottom",
     axis.ticks = element_line(colour = "grey70", size = 0.2),
     panel.grid.major = element_line(colour = "grey70", size = 0.2),
     panel.grid.minor = element_blank()
-  )+ggtitle("The profits of female entrepreneurs during the last monthl")
+  )+ggtitle("Monthly profit of the mobilized entrepreneurs")
++guides(fill=guide_legend(""))
  
